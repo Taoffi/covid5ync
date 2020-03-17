1a90946f74f280d2f5a9c86fa69999af93af3d60
taoffi <taoffi@toshiba12>
2020-03-17 16:10:59 +0100

    files update - change dev machine

--- covid5ync/controls/iDnaSequenceCtrl.xaml.cs
+++ covid5ync/controls/iDnaSequenceCtrl.xaml.cs
@@ -134,6 +134,7 @@ namespace iDna.controls
 
         public async void Zoom(object sender, MouseWheelEventArgs e)
         {
+            e.Handled   = false;
             if(_element == null)
                 return;
 
@@ -143,9 +144,12 @@ namespace iDna.controls
             {
                 var handle = (Keyboard.Modifiers & ModifierKeys.Control) > 0;
                 if (!handle)
+                {
                     return;
+                }
 
                 ApplyZoom(e.Delta);
+                e.Handled  = true;
             });
         }
 
